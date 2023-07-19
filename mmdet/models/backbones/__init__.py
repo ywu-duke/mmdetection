032a8a0593ed05bae309546977906ac1c9b8aa5c
Yuhao Wu <yuhao.wu@duke.edu>
2023-07-19 13:57:35 -0400

    Add ResNet from DenseCL

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -17,11 +17,12 @@ from .ssd_vgg import SSDVGG
 from .swin import SwinTransformer
 from .trident_resnet import TridentResNet
 from .vit_SelfPatch import SelfPatch_ViT
+from .resnet_densecl import ResNetDenseCL
 
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet',
     'MobileNetV2', 'Res2Net', 'HourglassNet', 'DetectoRS_ResNet',
     'DetectoRS_ResNeXt', 'Darknet', 'ResNeSt', 'TridentResNet', 'CSPDarknet',
     'SwinTransformer', 'PyramidVisionTransformer',
-    'PyramidVisionTransformerV2', 'EfficientNet', 'SelfPatch_ViT'
+    'PyramidVisionTransformerV2', 'EfficientNet', 'SelfPatch_ViT', 'ResNetDenseCL'
 ]
